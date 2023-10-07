5f252463b71e23ce2bd10fd1198c0363f3d43d6f
YooJinha96 <jinha_yoo@tmax.co.kr>
2023-10-07 12:47:19 +0900

    🚀 three Scene Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import styled from 'styled-components';
+import Scene from './components/THREE/Scene';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Serengeti Project, Lets go</h1>
-
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>Jinha, Sunny, Okorion {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to lea rn more</p>
-    </>
+    <Wrapper>
+      <Scene />
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+`;

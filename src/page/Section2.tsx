4df2f335e31783f51d79a484d545f469c784b08b
sunny <dance@kakao.com>
2023-10-08 18:59:05 +0900

    🚀각 섹션별 표시될 dummy text 추가

---
+++ src/page/Section2.tsx
@@ -0,0 +1,21 @@
+import Spacing from '../components/Spacing';
+
+const Section2 = () => {
+  return (
+    <div style={{ position: 'relative', zIndex: 2 }}>
+      <h1> Come here to follow</h1>
+      <Spacing size={1400} />
+      <h3> This is text</h3>
+      <h4>I think This is text</h4>
+      <h5>we think This is text... apple banana</h5>
+      <h5>we think This is text... cobra demon environment fuse gaia</h5>
+      <h5>we think This is text... head idle</h5>
+      <h5>we think This is text... jack kor</h5>
+      <h5>we think This is text</h5>
+      <h5>This is text!!</h5>
+      <Spacing size={2000} />
+    </div>
+  );
+};
+
+export default Section2;
---
+++ src/page/Section3.tsx
@@ -0,0 +1,12 @@
+import Spacing from '../components/Spacing';
+
+const Section3 = () => {
+  return (
+    <div style={{ position: 'relative', zIndex: 2 }}>
+      {/*empty page*/}
+      <Spacing size={2000} />
+    </div>
+  );
+};
+
+export default Section3;

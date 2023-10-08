0ae644d032545e815604e639d371531e590500f1
sunny <dance@kakao.com>
2023-10-08 19:05:41 +0900

    🚀스크롤에 따라 프로젝트의 화면과 section 별 텍스트의 내용이 지속적으로 바뀌도록 기능 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,51 @@
-import styled from 'styled-components';
+import { useRef } from 'react';
+import { useSectionRatio } from './hooks/use-section-ratio.ts';
 import Scene from './components/THREE/Scene';
+import Section1 from './page/Section1';
+import Section2 from './page/Section2';
+import Section3 from './page/Section3';
+import Section4 from './page/Section4';
+import Section5 from './page/Section5';
 
 function App() {
+  const appRef = useRef<HTMLDivElement>(null);
+  const section1Ref = useRef<HTMLDivElement>(null);
+  const section2Ref = useRef<HTMLDivElement>(null);
+  const section3Ref = useRef<HTMLDivElement>(null);
+  const section4Ref = useRef<HTMLDivElement>(null);
+  const section5Ref = useRef<HTMLDivElement>(null);
+
+  //[TBD] 추후 jotai를 이용한 전역스토어로 변경
+  const sectionRatio: Record<string, number> = {
+    first: useSectionRatio(appRef, section1Ref),
+    second: useSectionRatio(appRef, section2Ref),
+    third: useSectionRatio(appRef, section3Ref),
+    fourth: useSectionRatio(appRef, section4Ref),
+    fifth: useSectionRatio(appRef, section5Ref),
+  };
+
   return (
-    <Wrapper>
-      <Scene />
-    </Wrapper>
+    <div ref={appRef}>
+      <div ref={section1Ref}>
+        <Section1 />
+      </div>
+      <div ref={section2Ref}>
+        <Section2 />
+      </div>
+      <div ref={section3Ref}>
+        <Section3 />
+      </div>
+      <div ref={section4Ref}>
+        <Section4 />
+      </div>
+      <div ref={section5Ref}>
+        <Section5 />
+      </div>
+      {/*계산결과(sectionRatio)를 직접전달, 추후 jotai 적용*/}
+      {/*현재는 props drilling 이 너무 심함, App => Scene => useScrollDrivenCameraMovement */}
+      <Scene sectionRatio={sectionRatio} />
+    </div>
   );
 }
 
 export default App;
-
-const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: #000;
-`;
--- src/components/THREE/Scene.tsx
+++ src/components/THREE/Scene.tsx
@@ -1,25 +1,35 @@
 import Map from './Map';
-
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Euler, Vector3 } from 'three';
+import React from 'react';
+import { useScrollDrivenCameraMovement } from '../../hooks/use-scroll-driven-camera-movement.ts';
 
-const Scene = () => {
-  const position = new Vector3(-35.233871165688605, -24.030254877543264, 185.7260720836389);
-  const rotation = new Euler(0.1287806047801396, -0.1853436978655533, 0.0248930638674566);
+interface SceneProps {
+  sectionRatio: Record<string, number>;
+}
+
+const Scene: React.FC<SceneProps> = ({ sectionRatio }) => {
+  const { position, rotation } = useScrollDrivenCameraMovement({ sectionRatio });
 
   return (
     <>
-      <Canvas
-        style={{ width: '100vw', height: '100vh', position: 'fixed', left: 0, top: 0, backgroundColor: '#B8F5AE' }}
-      >
+      <Canvas style={canvasStyle}>
         <PerspectiveCamera makeDefault position={position} rotation={rotation} />
         <ambientLight />
         <directionalLight />
-        <OrbitControls />
         <Map />
       </Canvas>
     </>
   );
 };
 export default Scene;
+
+const canvasStyle: React.CSSProperties = {
+  zIndex: 1,
+  width: '100vw',
+  height: '100vh',
+  position: 'fixed',
+  left: 0,
+  top: 0,
+  backgroundColor: '#B8F5AE',
+};

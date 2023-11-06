2e63cdf4b683c153fe3fc4131d4f00b01fa8be1d
sunny <dance@kakao.com>
2023-11-06 18:51:12 +0900

    🚀캐릭터 이동 추가, scene에 캐릭터 이동 추가

--- src/components/THREE/Scene.tsx
+++ src/components/THREE/Scene.tsx
@@ -3,6 +3,8 @@ import { PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React from 'react';
 import { useScrollDrivenCameraMovement } from '../../hooks/use-scroll-driven-camera-movement.ts';
+import Character from './Character';
+import { useScrollDrivenCharacterMovement } from '../../hooks/use-scroll-driven-character-movement.ts';
 
 interface SceneProps {
   sectionRatio: Record<string, number>;
@@ -10,11 +12,13 @@ interface SceneProps {
 
 const Scene: React.FC<SceneProps> = ({ sectionRatio }) => {
   const { position, rotation } = useScrollDrivenCameraMovement({ sectionRatio });
+  const { characterPosition } = useScrollDrivenCharacterMovement({ sectionRatio });
 
   return (
     <>
       <Canvas style={canvasStyle}>
         <PerspectiveCamera makeDefault position={position} rotation={rotation} />
+        <Character position={characterPosition} />
         <ambientLight />
         <directionalLight />
         <Map />

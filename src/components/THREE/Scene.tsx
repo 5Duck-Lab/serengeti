d52ae093fd72a78f15a6b9162bfdeb6351e78d95
sunny <dance@kakao.com>
2023-11-07 21:04:09 +0900

    🔧Map 에 그림자 생길 수 있도록 변경

--- src/components/THREE/Scene.tsx
+++ src/components/THREE/Scene.tsx
@@ -16,11 +16,21 @@ const Scene: React.FC<SceneProps> = ({ sectionRatio }) => {
 
   return (
     <>
-      <Canvas style={canvasStyle}>
+      <Canvas shadows flat style={canvasStyle}>
         <PerspectiveCamera makeDefault position={position} rotation={rotation} />
         <Character position={characterPosition} />
         <ambientLight />
-        <directionalLight />
+        <directionalLight
+          castShadow
+          shadow-bias={-0.0001}
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+          position={[10, 18, 11.5]}
+          shadow-camera-left={-50}
+          shadow-camera-right={50}
+          shadow-camera-top={50}
+          shadow-camera-bottom={-50}
+        />
         <Map />
       </Canvas>
     </>

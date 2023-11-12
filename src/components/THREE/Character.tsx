eb1189628bbd80d2a1a0d9d55813b6fd8cf4b8e5
sunny <dance@kakao.com>
2023-11-12 13:37:35 +0900

    🚀scene 에서 계산된 rotation 값을 이용하여 캐릭터가 쳐다보는 방향으로 이동하는듯하게 보이도록 기능 추가

--- src/components/THREE/Character.tsx
+++ src/components/THREE/Character.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useGLTFLoader } from '@/hooks/use-loader';
 import { useCharacterAnimation } from '@/hooks/use-animation';
-import { Vector3 } from 'three';
+import { Vector3, Euler } from 'three';
 
 interface CharacterProps {
   position?: Vector3;
+  rotation?: Euler;
 }
 
-const Character: React.FC<CharacterProps> = ({ position = [-32, -28, -4] }) => {
+const Character: React.FC<CharacterProps> = ({
+  position = new Vector3(-32, -28, -4),
+  rotation = new Euler(0, 0, 0),
+}) => {
   const characterUrl = import.meta.env.VITE_GLB_URL as string;
   if (!characterUrl) {
     throw new Error('GLB URL is not defined, set your < .env > file');
@@ -16,7 +20,7 @@ const Character: React.FC<CharacterProps> = ({ position = [-32, -28, -4] }) => {
   const gltf = useGLTFLoader(characterUrl);
   useCharacterAnimation(gltf);
 
-  return <primitive object={gltf.scene} position={position} scale={[3, 3, 3]} />;
+  return <primitive object={gltf.scene} position={position} rotation={rotation} scale={[3, 3, 3]} />;
 };
 
 export default Character;

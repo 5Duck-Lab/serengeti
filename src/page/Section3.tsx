b319285d2f29a9fa15823a007e2e1f1d608123da
YooJinha96 <jinha_yoo@tmax.co.kr>
2023-11-16 01:10:12 +0900

    🚀 :  notion Page 가져오기

--- src/page/Section3.tsx
+++ src/page/Section3.tsx
@@ -1,10 +1,19 @@
-import Spacing from '@/components/Spacing';
-
+import { useEffect, useState } from 'react';
+import { NotionRenderer } from 'react-notion';
 const Section3 = () => {
+  const [recordMap, setRecordMap] = useState(null);
+  useEffect(() => {
+    const NOTION_PAGE_ID = '43cf367fd2e1492dac4363f7ee475fce';
+    fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
+      .then(res => res.json())
+      .then(resJson => {
+        setRecordMap(resJson);
+      });
+  }, []);
   return (
-    <div style={{ position: 'relative', zIndex: 2 }}>
-      {/*empty page*/}
-      <Spacing size={2000} />
+    <div style={{ position: 'relative', zIndex: 2, backgroundColor: 'white' }}>
+      {recordMap && <NotionRenderer blockMap={recordMap} fullPage={true} />}
+      {/* <Spacing size={2000} /> */}
     </div>
   );
 };

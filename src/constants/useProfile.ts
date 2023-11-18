3d4a9043563fa937540962f46d6a2203e4564a68
YooJinha96 <jinha_yoo@tmax.co.kr>
2023-11-19 01:12:02 +0900

    🔧 : 다시 추가

---
+++ src/constants/useProfile.ts
@@ -0,0 +1,28 @@
+export type UserProfileProps = {
+  title: string;
+  name: string;
+  education: string;
+  techStack: string;
+  career: string;
+  field: string;
+  number: string;
+  email: string;
+  notionPageId: string;
+  githubLink: string;
+  aboutMe: string;
+};
+
+export const USER_PROFILE: UserProfileProps = {
+  title: 'Serengeti 개발자 ',
+  name: '유진하',
+  education: '광운대학교 컴퓨터소프트웨어학부 졸업',
+  career: 'Tmax Metaverse',
+  field: 'Frontd',
+  techStack: 'react, javascript, three js',
+  number: '010-6553-1916',
+  email: 'yjs19162@daum.net',
+  notionPageId: '43cf367fd2e1492dac4363f7ee475fce',
+  githubLink: 'https://github.com/YooJinHa96/serengeti',
+  aboutMe:
+    '안녕하세요, 저는 Serengeti 개발자 유진하입니다.\n지금부터 제 소개를 시작해볼까 합니다.\n저에 대해 더 알고 싶으시다면\nScroll을 내려주세요.',
+};

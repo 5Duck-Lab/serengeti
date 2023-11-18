4c6f1dca7e61a37c6d1ae1054456469a73b44cd5
YooJinha96 <jinha_yoo@tmax.co.kr>
2023-11-19 01:01:22 +0900

    ♻️ : notionlink-> notionPageId로 key 변경

--- src/constants/useProfile.ts
+++ src/constants/useProfile.ts
@@ -7,7 +7,7 @@ export type UserProfileProps = {
   field: string;
   number: string;
   email: string;
-  notionLink: string;
+  notionPageId: string;
   githubLink: string;
   aboutMe: string;
 };
@@ -21,8 +21,7 @@ export const USER_PROFILE: UserProfileProps = {
   techStack: 'react, javascript, three js',
   number: '010-6553-1916',
   email: 'yjs19162@daum.net',
-  notionLink:
-    'https://www.notion.so/sunny-dance/7dfcd0b7f01642788092dbb309fc57c0?v=f412ef653955486e8a9816e0e4fe70e9&p=8cfa3552b76f48459e8ce86b01bb8b4f&pm=s',
+  notionPageId: '43cf367fd2e1492dac4363f7ee475fce',
   githubLink: 'https://github.com/YooJinHa96/serengeti',
   aboutMe:
     '안녕하세요, 저는 Serengeti 개발자 유진하입니다.\n지금부터 제 소개를 시작해볼까 합니다.\n저에 대해 더 알고 싶으시다면\nScroll을 내려주세요.',

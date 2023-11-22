3b13e5b899275ecf69ad8671d0faf11c14f9b36e
YooJinha96 <jinha_yoo@tmax.co.kr>
2023-11-23 00:07:17 +0900

    ♻️: useProfile 임시 수정

--- src/constants/useProfile.ts
+++ src/constants/useProfile.ts
@@ -14,15 +14,15 @@ export type UserProfileProps = {
 
 export const USER_PROFILE: UserProfileProps = {
   title: 'Serengeti 개발자 ',
-  name: '홍길동',
-  education: '고구려대학교 컴퓨터공학과 졸업',
-  career: '구글 수석 엔지니어 ',
-  field: 'Front-end',
+  name: '축얌',
+  education: '메이플대학 시그너스학과 소울마스터 졸업',
+  career: '레벨 277 달성\n무릉 61층\n 제네시스 무기 해방\n 유니온 8800\n 검은마법사 격파, 더 시드  50층 격파',
+  field: '시너지 딜러',
   techStack: 'react, javascript, typescript, html, css',
   number: '010-1234-5678',
   email: 'serengti@serengti.net',
-  notionPageId: 'pageId',
+  notionPageId: '9110d2048ce0460084a576c0b09c012c',
   githubLink: 'https://github.com/',
   aboutMe:
-    '안녕하세요, 저는 Serengeti 개발자 홍길동입니다.\n지금부터 제 소개를 시작해볼까 합니다.\n저에 대해 더 알고 싶으시다면\nScroll을 내려주세요.',
+    '안녕하세요, 저는 Serengeti 개발자 축얌입니다..\n지금부터 제 소개를 시작해볼까 합니다.\n저에 대해 더 알고 싶으시다면\nScroll을 내려주세요.',
 };

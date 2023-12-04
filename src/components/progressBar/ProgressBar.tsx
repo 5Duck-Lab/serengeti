e6a3832593608913d7059075e82e74b9e25548f3
okorion <ok.or.orion@gmail.com>
2023-12-04 21:28:55 +0900

    🔧: 진행도 컴포넌트 - attr과 소문자 prop명을 통해 console 에러 해결(scrollwidth, 200 class 생성)

--- src/components/progressBar/ProgressBar.tsx
+++ src/components/progressBar/ProgressBar.tsx
@@ -12,7 +12,7 @@ const fileNames = ['file1', 'file2', 'file3', 'file4', 'file5'];
 const ProgressBar: React.FC<ProgressBarProps> = ({ scrollFactor }) => {
   return (
     <ProgressBarShape>
-      <ProgressBarActive scrollFactor={scrollFactor} />
+      <ProgressBarActive $customwidth={scrollFactor} />
       <div style={{ display: 'flex', justifyContent: 'space-between', position: 'relative', top: '-17px' }}>
         {fileNames.map((fileName, index) => (
           <Checkpoint key={index} checked={index <= scrollFactor * (fileNames.length - 1)} />
--- src/components/progressBar/ProgressBarActive.tsx
+++ src/components/progressBar/ProgressBarActive.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface ProgressBarActiveProps {
-  scrollFactor: number;
+  $customwidth: number; // 변경된 prop 이름
 }
 
-const StyledProgressBarActive = styled.div<{ scrollWidth: number }>`
+const StyledProgressBarActive = styled.div.attrs<{ $customwidth: number }>(({ $customwidth }) => ({
+  style: {
+    width: `${$customwidth * 100}%`,
+  },
+}))<{ $customwidth: number }>`
   position: relative;
-  width: ${({ scrollWidth }) => scrollWidth * 100}%;
   height: 10px;
   background-color: yellow;
   z-index: 1;
 `;
 
-const ProgressBarActive: React.FC<ProgressBarActiveProps> = ({ scrollFactor }) => {
-  return <StyledProgressBarActive scrollWidth={scrollFactor} />;
+const ProgressBarActive: React.FC<ProgressBarActiveProps> = ({ $customwidth }) => {
+  return <StyledProgressBarActive $customwidth={$customwidth} />;
 };
 
 export default ProgressBarActive;
